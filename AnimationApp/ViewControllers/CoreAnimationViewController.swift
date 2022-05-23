b1a4d66155356a6937c39bfa3b116bd1f5d286cc
Kusakin_Sl <kusakin.slava@inbox.ru>
2022-05-23 23:56:42 +0300

    CoreAnimationViewController.swift: - completing the layout of the CoreAnimationViewController

--- AnimationApp/ViewControllers/CoreAnimationViewController.swift
+++ AnimationApp/ViewControllers/CoreAnimationViewController.swift
@@ -9,7 +9,19 @@ import UIKit
 
 class CoreAnimationViewController: UIViewController {
 
+    @IBOutlet weak var animationLabel: UILabel! {
+        didSet {
+            animationLabel.layer.masksToBounds = true
+            animationLabel.layer.cornerRadius = 15
+        }
+    }
+
     override func viewDidLoad() {
         super.viewDidLoad()
+    }
+
+
+
+    @IBAction func animationButtonTouchUp(_ sender: UIButton) {
     }
 }
