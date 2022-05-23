b23ea5cbb4a5458dac80a119a5aeeee433281c37
Kusakin_Sl <kusakin.slava@inbox.ru>
2022-05-23 23:39:10 +0300

    Create CoreAnimationViewController

---
+++ AnimationApp/ViewControllers/CoreAnimationViewController.swift
@@ -0,0 +1,15 @@
+//
+//  CoreAnimationViewController.swift
+//  AnimationApp
+//
+//  Created by Вячеслав Кусакин on 23.05.2022.
+//
+
+import UIKit
+
+class CoreAnimationViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
